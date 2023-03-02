e137b90ae828ac69e9cf022abef5a925f8ee7da4
Abdul Mboob <67025021+amboob223@users.noreply.github.com>
2023-03-01 22:44:29 -0500

    Add files via upload

--- server/db.js
+++ server/db.js
@@ -1,12 +1,9 @@
-const POOL = require("pg").Pool;
-
-   const pool = new POOL({
-    user:"crypto_olfk_user",
-    host:"dpg-cfvuvcqk728pn7u71dlg-a",
-    password:"HB8fdNb29gXBU1Rqi6NzDUEzFBCgxjdc",
-    database:"crypto_olfk",
-    port:5432
+const POOL = require("pg").Pool
+const pool = new POOL({
+    connectionString: 'postgres://crypto_olfk_user:HB8fdNb29gXBU1Rqi6NzDUEzFBCgxjdc@dpg-cfvuvcqk728pn7u71dlg-a.ohio-postgres.render.com/crypto_olfk',
+    ssl: {
+      rejectUnauthorized: false
+    }
 })
-;
 
 module.exports = pool;
