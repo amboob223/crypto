8518edb30450dc6224dfa814d8ac1508d7fde47c
Abdul Mboob <67025021+amboob223@users.noreply.github.com>
2023-03-01 19:44:45 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,12 @@
 const POOL = require("pg").Pool;
-const pool = new POOL({
-    connectionString:"postgresql://playabook:8896@localhost:5432/invest"});
+
+   const pool = new POOL({
+    user:"crypto_olfk_user",
+    host:"dpg-cfvuvcqk728pn7u71dlg-a",
+    password:"HB8fdNb29gXBU1Rqi6NzDUEzFBCgxjdc",
+    database:"crypto_olfk",
+    port:5432
+})
+;
 
 module.exports = pool;
